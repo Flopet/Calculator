05038038e141f2d807fee375c8bfcfb2f7a61137
Flopet17 <robbie@projectflop.com>
2015-09-24 22:06:05 -0400

    First Commit
    Nothing changed yet...

---
+++ script.js
@@ -0,0 +1,42 @@
+/*Numbers*/
+function run1(){document.calc.output.value += "1";}
+function run2(){document.calc.output.value += "2";}
+function run3(){document.calc.output.value += "3";}
+function run4(){document.calc.output.value += "4";}
+function run5(){document.calc.output.value += "5";}
+function run6(){document.calc.output.value += "6";}
+function run7(){document.calc.output.value += "7";}
+function run8(){document.calc.output.value += "8";}
+function run9(){document.calc.output.value += "9";}
+function run0(){document.calc.output.value += "0";}
+function runPL(){document.calc.output.value += "(";}
+function runPR(){document.calc.output.value += ")";}
+
+/*Operations*/
+function runPlus(){document.calc.output.value += "+";}
+function runMinus(){document.calc.output.value += "-";}
+function runMultiply(){document.calc.output.value += "*";}
+function runDivide(){document.calc.output.value += "/";}
+function equal(){
+	if (document.calc.output.value.length > 0){
+		document.calc.output.value += "==";
+	}else {
+		document.calc.output.value += "";
+	}
+}
+
+/*Other*/
+function clearcalc(){document.calc.output.value = "";}
+
+function doIt(){
+	var answer = document.calc.output.value;
+
+	var answer = eval(document.calc.output.value);
+	
+	document.calc.output.value = answer;
+
+}
+
+function back(){
+	document.calc.output.value = document.calc.output.value.substring(0, document.calc.output.value.length - 1); 
+}
